d9dd168fe004ee003a735f86aa7f704624672bb4
Dileep Parihar <179743478+Dileep2412@users.noreply.github.com>
2026-06-08 00:20:00 +0530

    Fix SMTP port 465

--- backend/mail.service.js
+++ backend/mail.service.js
@@ -2,11 +2,14 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GOOGLE_USER,
     pass: process.env.GOOGLE_APP_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
