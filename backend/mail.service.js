10578f98d8811ccd21b06069067642bc8f799d58
Dileep Parihar <179743478+Dileep2412@users.noreply.github.com>
2026-06-07 14:29:05 +0530

    Fix Gmail SMTP for Render deployment

--- backend/mail.service.js
+++ backend/mail.service.js
@@ -3,11 +3,13 @@ import nodemailer from "nodemailer";
 
 // Transporter
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.GOOGLE_USER,
-        pass: process.env.GOOGLE_APP_PASSWORD
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.GOOGLE_USER,
+    pass: process.env.GOOGLE_APP_PASSWORD,
+  },
 });
 
 // Verify Connection
